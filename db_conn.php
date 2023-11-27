65b511588760dae315955d369591ba2f151e4d0f
NamATruong <98953748+NamATruong@users.noreply.github.com>
2023-11-27 01:07:42 -0600

    Create db_conn.php

---
+++ db_conn.php
@@ -0,0 +1,14 @@
+<?php
+session_start();
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "udubhub";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+?>
