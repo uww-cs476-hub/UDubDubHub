faaf47ff592038caa64077e8a40857fa12e8faf6
NamATruong <namanhtruong@gmail.com>
2023-12-03 23:19:30 -0600

    Added tutor page

---
+++ tutor.php
@@ -0,0 +1,84 @@
+<?php
+session_start();
+include "db_conn.php";
+if (!$db) {
+    echo "Could not connect to database!";
+    exit();
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tutor Scheduling Service</title>
+    <link rel="stylesheet" href="tutor_style.css">
+</head>
+<body>
+<h1>Tutoring Service</h1>
+<div class="search-bar-tutor">
+    <input type="text" id="majorSearch" placeholder="Search for a major..." oninput="filterMajors()">
+</div>
+<div class="tutor-container">
+    <?php
+    $sql = "SELECT * FROM `major`";
+    $stm = $db->prepare($sql);
+    $stm->execute();
+    $majors = $stm->fetchAll(PDO::FETCH_ASSOC);
+
+    foreach ($majors as $major) {
+        $majorName = $major["name"];
+        $randomTutors = generateRandomTutors(); // Function to generate random tutor names
+
+        echo "<div class='course-card' id='major-$majorName'>";
+        echo "<h2 onclick='toggleTutors(\"$majorName\")'>$majorName</h2>";
+        echo "<div class='tutors' style='display: none;'>";
+        foreach ($randomTutors as $tutor) {
+            echo "<div class='tutor'>";
+            echo "<p>$tutor</p>";
+            echo "<button class='contact-button' onclick='contactTutor(\"$tutor\")'>Contact this tutor</button>";
+            echo "</div>";
+        }
+        echo "</div>";
+        echo "</div>";
+    }
+
+    function generateRandomTutors() {
+        $tutorNames = ["Spider-man", "Batman", "Harry Potter", "Barack Obama", "Donald Trump", "Walter White"];
+        shuffle($tutorNames);
+        return array_slice($tutorNames, 0, rand(1, count($tutorNames)));
+    }
+    ?>
+</div>
+
+<script>
+    function toggleTutors(majorName) {
+        var tutorsDiv = document.getElementById('major-' + majorName).getElementsByClassName('tutors')[0];
+        tutorsDiv.style.display = (tutorsDiv.style.display === 'none' || tutorsDiv.style.display === '') ? 'flex' : 'none';
+    }
+
+    function contactTutor(tutorName) {
+        alert("Contacting " + tutorName + ". Please check your email for further instructions.");
+    }
+
+    function filterMajors() {
+        var input, filter, cards, card, h2, i, txtValue;
+        input = document.getElementById("majorSearch");
+        filter = input.value.toUpperCase();
+        cards = document.getElementsByClassName("course-card");
+
+        for (i = 0; i < cards.length; i++) {
+            card = cards[i];
+            h2 = card.getElementsByTagName("h2")[0];
+            txtValue = h2.textContent || h2.innerText;
+
+            if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                card.style.display = "";
+            } else {
+                card.style.display = "none";
+            }
+        }
+    }
+</script>
+</body>
+</html>
