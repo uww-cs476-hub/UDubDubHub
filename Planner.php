f858b15472de6ce9cadd1d95b8bcbf11a31765dc
Ethan Hopp <emhopp97@gmail.com>
2023-12-03 21:14:41 -0600

    Update Styles

--- Planner.php
+++ Planner.php
@@ -52,7 +52,7 @@ if (isset($_SESSION["netID"])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Planner</title>
-    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="plannerStyles.css">
     <script src="plannerScript.js"></script>
 </head>
 
