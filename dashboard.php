89412680ac785f89c3b52e6fd10cce05e55ffc82
Jenna Lambert <57016635+TeslaPrince@users.noreply.github.com>
2023-12-04 11:41:03 -0600

    Update dashboard.php

--- dashboard.php
+++ dashboard.php
@@ -1,30 +1,102 @@
-<?php
-    session_start();
-    
-    include "header.php";
-
-    $modules = [
-        'module1' => true,
-    ];
-    $userRole = isset($_GET['role']) ? $_GET['role'] : 'guest';
-    // Include dashboard content based on the user's role
-    switch( $userRole ) {
-    case 'admin':
-        include 'admin_dashboard.php';
-        break;
-    case 'nf':
-        include 'newStudentFreshman.php';
-        break;
-    case 'soj':
-        include 'sophomoreJunior.php';
-        break;
-    case's+':
-        include 'senior.php';
-        break;
-    default:
-        header("Location: welcome.php");
-        break;
-    }
-
-    include 'footer.php';
-?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UFT-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="styles.css">
+        <?php include 'header.php'; ?>
+    </head>
+    <body>
+        <?php
+            $modules = [
+                'Get to Know Your Way Around Campus' => true,
+                'Helpful Academic Resources' => true,
+                'Campus Life Resources' => true,
+                'Other Resources' => true,
+                'Student Directory' => true,
+                'Get Involved'=> true,
+                'Neet Technical Help?'=> true,
+                'Graduation Resources'=> true,
+                'Career Resources'=> true,
+            ];
+            $userRole = isset($_GET['role']) ? $_GET['role'] : 'guest';
+            $active;
+            // Include dashboard content based on the user's role
+            switch( $userRole ) {
+            case 'admin':
+                $active = 'admin_dashboard.php';
+                $modules = [
+                    'Get to Know Your Way Around Campus' => true,
+                    'Helpful Academic Resources' => true,
+                    'Campus Life Resources' => true,
+                    'Other Resources' => true,
+                    'Student Directory' => true,
+                    'Get Involved'=> true,
+                    'Neet Technical Help?'=> true,
+                    'Graduation Resources'=> true,
+                    'Career Resources'=> true,
+                ];
+                break;
+            case 'nf':
+                $active = 'newStudentFreshman_dashboard.php';
+                $modules = [
+                    'Get to Know Your Way Around Campus' => true,
+                    'Helpful Academic Resources' => true,
+                    'Campus Life Resources' => true,
+                    'Other Resources' => true,
+                    'Student Directory' => true,
+                    'Get Involved'=> true,
+                    'Neet Technical Help?'=> true,
+                    'Graduation Resources'=> false,
+                    'Career Resources'=> false,
+                ];
+                break;
+            case 'soj':
+                $active = 'sophomoreJunior_dashboard.php';
+                $modules = [
+                    'Get to Know Your Way Around Campus' => false,
+                    'Helpful Academic Resources' => false,
+                    'Campus Life Resources' => true,
+                    'Other Resources' => false,
+                    'Student Directory' => true,
+                    'Get Involved'=> true,
+                    'Neet Technical Help?'=> true,
+                    'Graduation Resources'=> false,
+                    'Career Resources'=> false,
+                ];
+                break;
+            case's+':
+                $active = 'seniorPlus_dashboard.php';
+                $modules = [
+                    'Get to Know Your Way Around Campus' => false,
+                    'Helpful Academic Resources' => false,
+                    'Campus Life Resources' => false,
+                    'Other Resources' => true,
+                    'Student Directory' => true,
+                    'Get Involved'=> true,
+                    'Neet Technical Help?'=> true,
+                    'Graduation Resources'=> true,
+                    'Career Resources'=> true,
+                ];
+                break;
+            default:
+                $active = 'default_dashboard.php';
+                $modules = [
+                    'Get to Know Your Way Around Campus' => true,
+                    'Helpful Academic Resources' => true,
+                    'Campus Life Resources' => true,
+                    'Other Resources' => true,
+                    'Student Directory' => true,
+                    'Get Involved'=> true,
+                    'Neet Technical Help?'=> true,
+                    'Graduation Resources'=> true,
+                    'Career Resources'=> true,
+                ];
+                break;
+            }
+        ?>
+    </body>
+    <footer>
+        <?php include 'footer.php'; ?>
+    </footer>
+</html>
