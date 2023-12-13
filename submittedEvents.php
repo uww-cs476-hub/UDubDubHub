c34457176bf8103fc7fda34fe3e22dd4b577a327
Ethan Hopp <emhopp97@gmail.com>
2023-12-12 19:02:20 -0600

    Update submittedEvents.php

--- submittedEvents.php
+++ submittedEvents.php
@@ -1,5 +1,9 @@
 <?php
     session_start();
+    if (isset($_SESSION["isAdmin"]) && !$_SESSION["isAdmin"]) {
+        header("Location: modules.php");
+    }
+
     include "db_conn.php";
 
     $title = "Submitted Events";
